327e4b7a5b32c783bb2aab321a0844aa05187d06
maldini <tomoyaishigaki.cml@gmail.com>
2021-02-07 18:18:15 +0900

    ok

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 from app.app import app
+from flask import render_template
+
+
+@app.route('/')
+def index():
+    return render_template("season.html")
+
 
 if __name__ == '__main__':
     app.run()
